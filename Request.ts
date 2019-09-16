b90573d844ce32e3938727d0aefc95fa6656eba4
Simon Mika <simon@mika.se>
2019-09-16 17:58:49 +0200

    Fixed https Azure bug.

--- Request.ts
+++ Request.ts
@@ -11,6 +11,8 @@ export class Request implements servly.Request {
 	constructor(private readonly backend: HttpRequest) {
 		this.method = this.backend && this.backend.method || undefined
 		this.url = this.backend && this.backend.url || ""
+		if (this.url.startsWith("http://") && !this.url.startsWith("http://localhost")) // TODO: Fix for bug in Azure
+			this.url = "https:" + this.url.slice(5)
 		this.query = this.backend && this.backend.query || {}
 		this.parameter = this.backend && this.backend.params || {}
 		this.header = this.backend && servly.Request.Header.from(this.backend.headers) || {}
