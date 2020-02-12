335497b865ae8c9fcf3de3d5e8ee9a0cd642c998
Simon Mika <simonmika@users.noreply.github.com>
2020-02-12 18:59:51 +0100

    Updated dependencies and added handling of baseUrl. (#4)

--- Request.ts
+++ Request.ts
@@ -1,9 +1,11 @@
+import * as process from "process"
 import { HttpRequest } from "@azure/functions"
 import * as servly from "servly"
 
 export class Request implements servly.Request {
 	readonly method: servly.Request.Method | undefined
 	readonly url: string
+	readonly baseUrl: string
 	readonly query: { [key: string]: string; }
 	readonly parameter: { [key: string]: string; }
 	get remote(): string | undefined { return (this.backend.params.MS_HttpContext as any as { request: { userHostAddress: string } })?.request?.userHostAddress }
@@ -12,8 +14,11 @@ export class Request implements servly.Request {
 	constructor(private readonly backend: HttpRequest) {
 		this.method = this.backend && this.backend.method || undefined
 		this.url = this.backend && this.backend.url || ""
-		if (this.url.startsWith("http://") && !this.url.startsWith("http://localhost")) // TODO: Fix for bug in Azure
+		if (process.env.baseUrl)
+			this.url = process.env.baseUrl + (this.url && this.url.slice(this.url.split("/", this.url.includes("//") ? 3 : 1).join("/").length))
+		else if (this.url.startsWith("http://") && !this.url.startsWith("http://localhost")) // TODO: Fix for bug in Azure
 			this.url = "https:" + this.url.slice(5)
+		this.baseUrl = this.url.split("/", this.url.includes("//") ? 3 : 1).join("/")
 		this.query = this.backend && this.backend.query || {}
 		this.parameter = this.backend && this.backend.params || {}
 		this.header = this.backend && servly.Request.Header.from(this.backend.headers) || {}
--- index.spec.ts
+++ index.spec.ts
@@ -4,40 +4,72 @@ import * as azure from "./index"
 
 describe("servly-azure", () => {
 	const endpoint = servly.Endpoint.create(async request => ({
-		body: { name: "servly" },
+		body: { name: "servly", url: request.url, baseUrl: request.baseUrl },
 	}))
 	const run = azure.eject(endpoint)
-	it("test", async () => {
-		const req: HttpRequest = { url: "test.com", method: "GET", headers: {}, query: {}, params: {} }
-		const context: Context = {
-			req,
+	const req: HttpRequest = { url: "http://test.com/folder/file.extension", method: "GET", headers: {}, query: {}, params: {} }
+	const context: Context = {
+		req,
+		invocationId: "1337",
+		executionContext: {
 			invocationId: "1337",
-			executionContext: {
-				invocationId: "1337",
-				functionName: "run",
-				functionDirectory: "./",
-			},
-			bindings: {},
-			bindingData: {},
-			bindingDefinitions: [],
-			log: {
-				...Object.assign((..._: any[]) => { }),
-				error: (..._: any[]) => { },
-				warn: (..._: any[]) => { },
-				info: (..._: any[]) => { },
-				verbose: (..._: any[]) => { },
-			},
-			done: (_?: string | Error | null, result?: any) => Promise.resolve(result),
-		}
+			functionName: "run",
+			functionDirectory: "./",
+		},
+		bindings: {},
+		bindingData: {},
+		bindingDefinitions: [],
+		log: {
+			...Object.assign((..._: any[]) => { }),
+			error: (..._: any[]) => { },
+			warn: (..._: any[]) => { },
+			info: (..._: any[]) => { },
+			verbose: (..._: any[]) => { },
+		},
+		done: (_?: string | Error | null, result?: any) => Promise.resolve(result),
+	}
+	it("http", async () => {
 		await run(context, req)
 		expect(context.res).toEqual({
 			body: {
 				name: "servly",
+				url: "https://test.com/folder/file.extension",
+				baseUrl: "https://test.com",
 			},
 			headers: {
 				"Content-Type": "application/json; charset=utf-8",
 			},
 			status: 200,
 		})
+	})
+	it("http://localhost", async () => {
+		await run(context, { ...req, url: "http://localhost:1337/folder/file.extension" })
+		expect(context.res).toEqual({
+			body: {
+				name: "servly",
+				url: "http://localhost:1337/folder/file.extension",
+				baseUrl: "http://localhost:1337",
+			},
+			headers: {
+				"Content-Type": "application/json; charset=utf-8",
+			},
+			status: 200,
+		})
+	})
+	it("http://localhost", async () => {
+		process.env.baseUrl = "https://example.com"
+		await run(context, { ...req, url: "http://localhost:1337/folder/file.extension" })
+		expect(context.res).toEqual({
+			body: {
+				name: "servly",
+				url: "https://example.com/folder/file.extension",
+				baseUrl: "https://example.com",
+			},
+			headers: {
+				"Content-Type": "application/json; charset=utf-8",
+			},
+			status: 200,
+		})
+		delete process.env.baseUrl
 	})
 })
