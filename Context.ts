a8ef8d02929601580d1e98cdc7e5dc920b655956
Simon Mika <simon@mika.se>
2020-04-20 18:52:33 +0200

    Added context logging.

--- Context.ts
+++ Context.ts
@@ -10,12 +10,29 @@ export namespace Context {
 				name: context.executionContext.functionName,
 				path: context.executionContext.functionDirectory,
 			},
-			log: (step: string, level: servly.Log.Level, content: servly.Log.Content) => log.entries.push({
-				created: isoly.DateTime.now(),
-				step,
-				level,
-				content,
-			}),
+			log: (step: string, level: servly.Log.Level, content: servly.Log.Content) => {
+				switch (level) {
+					case "trace":
+						context.log.info(step, level, content)
+						break
+					case "debug":
+						context.log.verbose(step, level, content)
+						break
+					case "warning":
+						context.log.warn(step, level, content)
+						break
+					case "error":
+					case "fatal":
+						context.log.error(step, level, content)
+						break
+				}
+				log.entries.push({
+					created: isoly.DateTime.now(),
+					step,
+					level,
+					content,
+				})
+			},
 			callback: (r: servly.Request) => callback.push(r),
 		})
 	}
