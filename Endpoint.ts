8105aad492f0241d5acc3598c21a522fc29b6f67
robinnedergard <robin.nedergard@payfunc.com>
2023-10-17 18:43:27 +0200

    feat: module fixes

--- Endpoint.ts
+++ Endpoint.ts
@@ -3,24 +3,29 @@ import * as azure from "@azure/functions"
 import { Context } from "./Context"
 import { Request } from "./Request"
 
-export const eject: servly.Function.Ejector<azure.AzureFunction> = (handler: servly.Endpoint) => {
-	return async (context: azure.Context, request: azure.HttpRequest) => {
+export const eject: servly.Function.Ejector<azure.FunctionHandler> = (handler: servly.Endpoint) => {
+	return async (request: azure.HttpRequest, context: azure.InvocationContext) => {
 		const log: servly.Log = {
-			invocation: context.executionContext.invocationId,
-			point: context.executionContext.functionName,
+			invocation: context.invocationId,
+			point: context.functionName,
 			entries: [],
 		}
 		const callback: servly.Request[] = []
 		const c = Context.create(context, log, callback)
 		const response = await handler(c, new Request(request))
-		context.res = {
+		const meta = servly.Meta.freeze(c.meta)
+		if (log.entries.length > 0)
+			context.extraInputs.set("servlyLog", { ...log, ...meta })
+		if (callback.length > 0)
+			context.extraInputs.set(
+				"servlyCallback",
+				callback.map(cb => ({ ...cb, meta }))
+			)
+		return {
 			status: response.status,
 			headers: servly.Response.Header.to(response.header),
 			body: response.body,
 			isRaw: true,
 		}
-		const meta = servly.Meta.freeze(c.meta)
-		context.bindings.log = log.entries.length > 0 ? { ...log, ...meta } : undefined
-		context.bindings.callback = callback.length > 0 ? callback.map(cb => ({ ...cb, meta })) : undefined
 	}
 }
